533c1a367fe1c7f8efbf3758bcd5790caf18f9f4
labso08 <labso08@labso08-All-Series>
2022-03-09 08:27:57 -0600

    Este es el segun comit en el que se integran la clase conexion y se integran las diferentes clases que tienes que ver con la conexion hacia la base de datos

--- Casa$/src/java/db/Conexion.java
+++ Casa$/src/java/db/Conexion.java
@@ -1,7 +1,7 @@
 /*
  Autor: Gamaliel Silva Lopez
  Fecha de creacion: 7 de marzo de 2022
- Fecha de modificacion: 8 de marzo de 2022
+ Fecha de modificacion: 9 de marzo de 2022
  Descripcion: Clase conexion
  */
 package db;
@@ -10,9 +10,9 @@ import java.sql.*;
 
 public class Conexion {
 
-    Connection conexion;
-    String url = "jdbc:postgresql://localhost/";
-    String user = "postgrest";
+    static Connection conexion;
+    String url = "jdbc:postgresql://localhost/casas";
+    String user = "postgres";
     String password = "HOLA";
 
     public Conexion() {
@@ -26,21 +26,54 @@ public class Conexion {
         this.password = password;
     }
 
-    public void Conectar() {
+    public Connection Conectar() {
+        Connection con = null;
         try {
-            Class.forName("org.postgrest.Driver");
-            conexion = DriverManager.getConnection(url, user, password);
+            Class.forName("org.postgresql.Driver");
+            conexion = DriverManager.getConnection(url,user,password);
+            System.out.println("Conexion con exito");
         } catch (Exception e) {
             System.out.println(e.toString());
         }
+        return con;
     }
 
     public void Desconectar() {
+        try {
+            conexion.close();
+            System.out.println("Conexion cerrada");
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
 
     }
 
     public static void main(String[] args) {
 
+        Conexion con = new Conexion();
+        con.Conectar();
+        con.Desconectar();
+//        try{
+//          
+//            String sql="select * from casas";
+//             ResultSet rs;
+//             Statement st= con.createStatement();
+//             rs=st.executeQuery(sql);
+//             while(rs.next()){
+//                 System.out.println(rs.getObject("codigo"));
+//                 System.out.println(rs.getObject("nombre"));
+//                 System.out.println(rs.getObject("descripcion"));
+//                 System.out.println(rs.getObject("precio"));
+//                 System.out.println(rs.getObject("FechaCreacion"));
+//                 System.out.println(rs.getObject("FechaActualizacion"));
+//                 System.out.println(rs.getObject("FechaEliminacion"));
+//                 
+//                 
+//             }
+//             
+//        }catch(Exception e){
+//            System.out.println(e.toString());
+//        }
     }
 
 }
